6a1fc9b497b8ef50525a40c06b6fd47e95ca74be
Roman Koshchei <romankoshchei@gmail.com>
2022-12-14 11:23:52 +0200

    make more rust

--- example/servers/main/index.rs
+++ example/servers/main/index.rs
@@ -15,4 +15,14 @@ Can make them under group route to seperate from extra functionalty:
   /extra
     /special-func
 
+It will need not full orm, because our extra functionality only use authors table.
+That's cool because we don't keep unnesassary code in our proj.
+Ozen is cool, because we can easily combine langs to work with db.
+For example ozen handler in rust, but extra functionality in ts (because web dev).
+And we can use any db connector, for example planetscale js lib for sending req to db.
+
 */
+
+fn main() {
+    println!("Ozen!");
+}
